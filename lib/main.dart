b54341a586d4755c20be914b5b26d5e94f5be59a
singh-that-codes <japjeets2003@gmail.com>
2024-02-21 15:45:20 +0530

    Firebase Auth Configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tweet_app/design/app_theme.dart';
 import 'package:tweet_app/features/onboarding/ui/onbaording_sceen.dart';
+import 'package:tweet_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
