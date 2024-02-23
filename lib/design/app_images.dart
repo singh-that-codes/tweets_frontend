2979ce304c2a7d1da6409d71f0a07ab6d252b6f7
singh-that-codes <japjeets2003@gmail.com>
2024-02-23 10:40:08 +0530

    Register page UI done

--- lib/design/app_images.dart
+++ lib/design/app_images.dart
@@ -2,4 +2,6 @@ class AppImages {
   static const String leaf = "assets/images/leaf.jpg";
   static const String person = "assets/images/person.jpg";
   static const String sky = "assets/images/sky.jpg";
+  static const String logo = "assets/images/tweetz.png";
+  static const String google = "assets/images/google.png";
 }
--- lib/features/auth/ui/auth_register_screen.dart
+++ lib/features/auth/ui/auth_register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tweet_app/design/app_images.dart';
 
 class AuthRegisterScreen extends StatefulWidget {
   const AuthRegisterScreen({super.key});
@@ -10,6 +11,92 @@ class AuthRegisterScreen extends StatefulWidget {
 class _AuthRegisterScreenState extends State<AuthRegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 80, bottom: 30),
+              child: Image.asset(AppImages.logo),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Forgot Password"),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 45,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(2.0), // No border radius
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text("New to tweetz ? Register Now!"),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(2.0), // No border radius
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      AppImages.google,
+                      height: 30,
+                    ),
+                    Text("Sign in with Google"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
